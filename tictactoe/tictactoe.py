ba9185ac1cea041351a9f8bd8fe552db35bcefb7
ankurkhetan2015 <ankurkhetan2615@gmail.com>
2020-06-26 13:13:02 -0500

    Added basic instruction and displaying game board

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -11,7 +11,11 @@ def display_Board(current_board):
     '''
     displays the current game state to user
     '''
-    pass
+    print(current_board['q']+' | '+current_board['w']+' | '+current_board['e'])
+    print('--+---+--')
+    print(current_board['a']+' | '+current_board['s']+' | '+current_board['d'])
+    print('--+---+--')
+    print(current_board['z']+' | '+current_board['x']+' | '+current_board['c'])
 
 
 def user_choice(check_choice):
@@ -46,7 +50,38 @@ def start_game():
     '''
     the game logic resides here
     '''
-    pass
+
+    '''
+    display the instruction for the players to know how to play
+    '''
+    instruction_board = [['q', 'w', 'e'], ['a', 's', 'd'], ['z', 'x', 'c']]
+    '''
+    initially game_on is True as the players play the first game
+    '''
+    game_on = True
+
+    print('\nThe following keyboard keys represent the box to put your mark "X" or "O" \n')
+
+    for i, rows in enumerate(instruction_board):
+        for j, cols in enumerate(rows):
+            if j < 2:
+                print(cols + ' |', end=' ')
+            else:
+                print(cols, end=' ')
+        print()
+        if i < 2:
+            print('--+---+--')
+
+    while game_on:
+        '''
+        board is a dictionary where each key corresponds to a button on keyboard that needs to be pressed to fill in that space
+
+        initially the board is empty
+        '''
+        board = {'q': ' ', 'w': ' ', 'e': ' ', 'a': ' ', 's': ' ', 'd': ' ', 'z': ' ', 'x': ' ', 'c': ' '}
+
+        print('\nPlayer1(X) your turn:\n')
+        display_Board(board)
 
 
 start_game()
